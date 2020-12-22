e09ff7b10757666b91945ac5dc4077107e60dda6
Eider Arango <eider.a@hotmail.com>
2020-12-21 22:50:47 -0500

    added pymongo

--- db/expense_db.py
+++ db/expense_db.py
@@ -1,6 +1,20 @@
 from typing import Dict
 from pydantic import BaseModel
+from pymongo import MongoClient
+from bson.json_util import dumps
+from bson.json_util import loads
 
+MONGO_URI = 'mongodb+srv://root:root@cluster0.3idi6.mongodb.net/expenses?retryWrites=true&w=majority'
+
+client = MongoClient(MONGO_URI)
+
+db = client['mintic_db']
+collection = db['expenses']
+
+# collection.insert_one({"username":"andres18", "title": "Nuevo TV", "value":15000, "description": "Hoy compre nuevo tv", "payment_type": "efectivo"})
+# collection.insert_one({"username":"camilo24", "title": "Arriendo", "value":250000, "description": "Pago del arriendo", "payment_type": "efectivo"})
+# collection.insert_one({"username":"camilo24", "title": "Play station plus", "value":35000, "description": "Pago de mensualidad de play plus", "payment_type": "tarjeta de credito"})
+# collection.insert_one({"username":"andres18", "title": "Horno", "value":450000, "description": "Horno LG estaba en promoción", "payment_type": "tarjeta de credito"})
 
 class ExpenseInDB(BaseModel):
     username: str
@@ -20,23 +34,41 @@ database_expenses = [
 ]
 
 def get_expenses_by_username(username: str):
-    match = []
-    print(database_expenses)
-    print('**********antes de buscar')
-    for x in database_expenses:
-        print(x.username)
-        if x.username == username:
-            match.append(x)
+    # match = []
+    # print(database_expenses)
+    # print('**********antes de buscar')
+    # for x in database_expenses:
+    #     print(x.username)
+    #     if x.username == username:
+    #         match.append(x)
     
-    return match
+    # return match
+
+
+    results = collection.find({"username": username}, {'_id': 0})
+    
+    return list(results)
 
 
 def register_new_expense(expense_in_db: ExpenseInDB):
    
-    database_expenses.append(expense_in_db)
+    # database_expenses.append(expense_in_db)
 
-    print(database_expenses)
+    # print(database_expenses)
+    # return expense_in_db
+
+
+    data = {
+      "username":      expense_in_db.username,
+      "title":      expense_in_db.title,
+      "value":      expense_in_db.value,
+      "description":      expense_in_db.description,
+      "payment_type":      expense_in_db.payment_type,
+    }
+
+    collection.insert_one(data)
     return expense_in_db
+
 
 def get_all_expenses():
    
